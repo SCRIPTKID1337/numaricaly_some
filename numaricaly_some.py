acead7d8a8fd2113b3bd67db4f2668c0c0f628d5
scriptkid <scriptkid1337@gmail.com>
2020-08-30 21:29:50 +0530

    first commit

---
+++ numaricaly_some.py
@@ -0,0 +1,7 @@
+total = 0
+first_number = int(input("enter first number : "))
+second_number = int(input("enter second number : "))
+while first_number<= second_number:
+    total = total +first_number
+    first_number= first_number+1
+print(total)    
